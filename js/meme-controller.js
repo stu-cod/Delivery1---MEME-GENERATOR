73e93a67c45a833fc4f8e911b2b3e692ce34860f
stu-cod <stavohana8@gmail.com>
2024-12-27 19:10:01 +0200

    add a text box

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -18,5 +18,32 @@ function drewImg({ imgUrl }) {
   img.onload = () => {
     gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
     gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+    drawText('hey', 200, 200)
   }
+}
+
+function drawText(text, x, y, fontSize = 40) {
+  gCtx.lineWidth = 2
+  gCtx.fillStyle = 'black'
+  gCtx.font = fontSize + 'px Arial'
+
+  gCtx.fillText(text, x, y)
+
+  const textMetrics = gCtx.measureText(text)
+  const textWidth = textMetrics.width
+  const textHeight = fontSize
+
+  drawFrameAroundText({ x, y, width: textWidth, height: textHeight })
+}
+
+function drawFrameAroundText(textBounds, padding = 10) {
+  const { x, y, width, height } = textBounds
+
+  const boxX = x - padding
+  const boxY = y - height - padding / 2
+  const boxWidth = width + padding * 2
+  const boxHeight = height + padding * 2
+
+  gCtx.strokeStyle = 'black'
+  gCtx.strokeRect(boxX, boxY, boxWidth, boxHeight)
 }
