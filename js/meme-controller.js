47973028836788f38f9ffaf5f5d4c9a8b70a4aee
stu-cod <stavohana8@gmail.com>
2024-12-27 23:07:37 +0200

    Add multiple lines support

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -19,28 +19,30 @@ function drewImg({ imgUrl, lines }) {
   img.onload = () => {
     gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
     gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
-
-    drawText(gElCanvas.width / 2, gElCanvas.height / 2, lines[0])
+    drawText(gElCanvas.width / 2, gElCanvas.height / 2, lines)
   }
 }
 
-function drawText(x, y, { txt, size, color }) {
-  gCtx.lineWidth = 2
-  gCtx.fillStyle = color
-  gCtx.font = size + 'px Arial'
+function drawText(x, y, lines) {
+  lines.forEach((line, idx) => {
+    const { txt, size, color } = line
+    gCtx.lineWidth = 2
+    gCtx.fillStyle = color
+    gCtx.font = size + 'px Arial'
 
-  gCtx.fillText(txt, x, y)
+    gCtx.fillText(txt, x, y)
 
-  const txtMetrics = gCtx.measureText(txt)
-  const txtWidth = txtMetrics.width
-  const txtHeight = size
+    const txtMetrics = gCtx.measureText(txt)
+    const txtWidth = txtMetrics.width
+    const txtHeight = size
 
-  drawFrameAroundText({ x, y, width: txtWidth, height: txtHeight })
+    if (idx === gMeme.selectedLineIdx) {
+      drawFrameAroundText(x, y, txtWidth, txtHeight)
+    }
+  })
 }
 
-function drawFrameAroundText(txtBounds, padding = 10) {
-  const { x, y, width, height } = txtBounds
-
+function drawFrameAroundText(x, y, width, height, padding = 8) {
   const boxX = x - padding
   const boxY = y - height - padding / 2
   const boxWidth = width + padding * 2
@@ -50,8 +52,14 @@ function drawFrameAroundText(txtBounds, padding = 10) {
   gCtx.strokeRect(boxX, boxY, boxWidth, boxHeight)
 }
 
-function onSubmit(ev) {
-  ev.preventDefault()
+function onAddTxtLine() {
+  addTxtLine()
+  renderMeme()
+}
+
+function onChangeTxtLine() {
+  changeTxtLine()
+  renderMeme()
 }
 
 function onEditTxt(elTxt) {
--- js/meme-service.js
+++ js/meme-service.js
@@ -32,15 +32,36 @@ function _createMeme(imgId) {
   }
 }
 
+function addTxtLine() {
+  var line = _createLine()
+  gMeme.lines.unshift(line)
+}
+
+function _createLine() {
+  return {
+    txt: 'your text',
+    size: 40,
+    color: '#ffcc00',
+  }
+}
+
+function changeTxtLine() {
+  const linesNum = gMeme.lines.length
+  if (linesNum < 1) return
+
+  if (gMeme.selectedLineIdx < linesNum - 1) gMeme.selectedLineIdx++
+  else gMeme.selectedLineIdx = 0
+}
+
 function setLineTxt(txt) {
-  gMeme.lines[0].txt = txt
+  gMeme.lines[gMeme.selectedLineIdx].txt = txt
 }
 
 function setLineColor(color) {
-  gMeme.lines[0].color = color
+  gMeme.lines[gMeme.selectedLineIdx].color = color
 }
 
 function setFontSize(fontChange) {
-  if (fontChange === 'up') gMeme.lines[0].size++
-  else gMeme.lines[0].size--
+  if (fontChange === 'up') gMeme.lines[gMeme.selectedLineIdx].size++
+  else gMeme.lines[gMeme.selectedLineIdx].size--
 }
